916b6bd741ee526220656c4fc77c7d881ce97cde
diegotorres9 <diegoetorres1@gmail.com>
2022-07-07 19:16:13 -0400

    working on trending page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import data from "../data.json";
 import Searchbar from "./Searchbar";
 
 class Home extends React.Component {
@@ -6,8 +7,42 @@ class Home extends React.Component {
         return(
             <div>
             <Searchbar />
-                <h1>Trending</h1>
-                <h2>Recommended for you</h2>
+                <h1 className="page-title">Trending</h1>
+                <div className="trending">
+                    {
+                        data.filter(trending => (trending.isTrending === true)
+                        ).map((trending, key) => (
+                            <div className="grid-item" key={key}>
+                            <img 
+                                className='grid-image'
+                                src={trending.thumbnail.regular.small}
+                                srcSet={`
+                                ${trending.thumbnail.regular.small} 300w,
+                                ${trending.thumbnail.regular.medium} 768w,
+                                ${trending.thumbnail.regular.large} 1280w
+                                `}
+                                alt={"A still from " + trending.title + "."}  
+                            />
+                                <div className="details">
+                                    <p>{trending.year}</p>
+                                    <span className="seperator"></span>
+                                    <svg 
+                                    className='category-icon'
+                                    width="12" 
+                                    height="12" 
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M10.173 0H1.827A1.827 1.827 0 0 0 0 1.827v8.346C0 11.183.818 12 1.827 12h8.346A1.827 1.827 0 0 0 12 10.173V1.827A1.827 1.827 0 0 0 10.173 0ZM2.4 5.4H1.2V4.2h1.2v1.2ZM1.2 6.6h1.2v1.2H1.2V6.6Zm9.6-1.2H9.6V4.2h1.2v1.2ZM9.6 6.6h1.2v1.2H9.6V6.6Zm1.2-4.956V2.4H9.6V1.2h.756a.444.444 0 0 1 .444.444ZM1.644 1.2H2.4v1.2H1.2v-.756a.444.444 0 0 1 .444-.444ZM1.2 10.356V9.6h1.2v1.2h-.756a.444.444 0 0 1-.444-.444Zm9.6 0a.444.444 0 0 1-.444.444H9.6V9.6h1.2v.756Z" 
+                                    fill="#FFF"
+                                    opacity=".75"
+                                    />
+                                    </svg>
+                                </div>
+                            </div>
+
+                        ))
+                    }
+                </div>
+                <p className="page-title">Recommended for you</p>
             </div>
         )
     }
